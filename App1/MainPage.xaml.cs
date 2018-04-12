8c7566f7b6e265f5b2522c00f15a95bd1d30a021
Administrator <Administrator@DESKTOP-3US5H87>
2018-04-12 18:00:32 +0200

    Add project files.

---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,115 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Data.Json;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+namespace App1
+{
+
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+
+    }
+
+
+    
+    public sealed partial class MainPage : Page
+    {
+   //     private static string JsonFile =
+   //"C:\\Users\\Administrator\\AppData\\Local\\Packages\\3cb18cff-163c-452c-840c-f8bcc5eda7a9_75cr2b68sm664\\LocalState\\sample.txt";
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            DataContext = ReadFromJSON();
+
+            //System.IO.File.WriteAllText(@"C:\temp\personaldata1.txt", json);
+
+        }
+
+        public async static void SaveToJSON(string json)
+        {
+
+            // Create sample file; replace if exists.
+            Windows.Storage.StorageFolder storageFolder =
+                    Windows.Storage.ApplicationData.Current.LocalFolder;
+
+            Windows.Storage.StorageFile sampleFile =
+                    await storageFolder.CreateFileAsync("sample.txt",
+                        Windows.Storage.CreationCollisionOption.ReplaceExisting);
+
+            await Windows.Storage.FileIO.WriteTextAsync(sampleFile, json);
+        }
+        
+        public static JArray ReadFromJSON() {
+            var json = System.IO.File.ReadAllText(@"C:\Users\Administrator\AppData\Local\Packages\3cb18cff-163c-452c-840c-f8bcc5eda7a9_75cr2b68sm664\LocalState\sample.json");
+
+            json = json.Replace(System.Environment.NewLine, "");
+
+            JArray objects = JArray.Parse(json); 
+
+            //foreach (var person in objects)
+            //{
+
+            //    var id = person["ID"];
+            //    var name = person["Name"];
+            //    var description = (String)person["Description"];
+                
+            //}
+
+
+            return objects;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            List<Person> _data = new List<Person>
+            {
+                new Person(){
+                    ID = 1,
+                    Name = "Name01",
+                    Description = "Desc01"
+                },
+                new Person(){
+                    ID = 2,
+                    Name = "Name02",
+                    Description = "Desc02"
+                }
+            };
+
+            string json = JsonConvert.SerializeObject(_data.ToArray(), Formatting.Indented);
+
+            SaveToJSON(json);
+
+        }
+
+        private void Read_Button(object sender, RoutedEventArgs e)
+        {
+
+            ReadFromJSON();
+
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,118 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> _data = new List<Person>
+            {
+                new Person(){
+                    ID = 1,
+                    Name = "Name01",
+                    Description = "Desc01"
+                },
+                new Person(){
+                    ID = 2,
+                    Name = "Name02",
+                    Description = "Desc02"
+                }
+            };
+
+            string json = JsonConvert.SerializeObject(_data.ToArray(), Formatting.Indented);
+
+            //SaveToJSON(json);
+
+            ReadFromJSON();
+        }
+
+        public static void SaveToJSON(string json)
+        {
+
+
+            //open file stream
+            using (StreamWriter file = File.CreateText(@"C:\Users\Administrator\AppData\Local\Packages\3cb18cff-163c-452c-840c-f8bcc5eda7a9_75cr2b68sm664\LocalState\sample2.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                //serialize object directly into file stream
+                serializer.Serialize(file, json);
+            }
+        }
+
+        //public static void ReadFromJSON() {
+
+
+        //    var json = System.IO.File.ReadAllText(@"C:\Users\Administrator\AppData\Local\Packages\3cb18cff-163c-452c-840c-f8bcc5eda7a9_75cr2b68sm664\LocalState\new.json");
+
+        //    var objects = JArray.Parse(json); // parse as array  
+        //    foreach (JObject root in objects)
+        //    {
+        //        foreach (KeyValuePair<String, JToken> app in root)
+        //        {
+        //            var appName = app.Key;
+        //            var description = (String)app.Value["Description"];
+        //            var value = (String)app.Value["Value"];
+
+        //            Console.WriteLine(appName);
+        //            Console.WriteLine(description);
+        //            Console.WriteLine(value);
+        //            Console.WriteLine("\n");
+        //        }
+        //    }
+
+        //    Console.ReadKey();
+        //}
+
+        public static void ReadFromJSON()
+        {
+            //var json = System.IO.File.ReadAllText(@"C:\Users\Administrator\AppData\Local\Packages\3cb18cff-163c-452c-840c-f8bcc5eda7a9_75cr2b68sm664\LocalState\sample2.txt");
+            //List<Person> deserializedJson = JsonConvert.DeserializeObject<List<Person>>(json);
+
+            var json = System.IO.File.ReadAllText(@"C:\Users\Administrator\AppData\Local\Packages\3cb18cff-163c-452c-840c-f8bcc5eda7a9_75cr2b68sm664\LocalState\sample.json");
+            json = json.Replace(System.Environment.NewLine, "" );
+
+            JArray objects = JArray.Parse(json); // parse as array  
+
+            foreach (var person in objects)
+            {
+
+                    var id = person["ID"];
+                    var name = person["Name"];
+                    var description = (String)person["Description"];
+
+                    Console.WriteLine(id);
+                    Console.WriteLine(name);
+                    Console.WriteLine(description);
+                    Console.WriteLine("\n");
+
+               
+            }
+
+
+
+            Console.ReadKey();
+            //return deserializedJson;
+        }
+
+
+    }
+}
